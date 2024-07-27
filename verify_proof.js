f778dcd4becdc93abc0bae640d7d677b09c0c23f
lancenonce <lancendavis47@gmail.com>
2024-07-27 10:11:15 +0000

    add off chain verification

--- verify_proof.js
+++ verify_proof.js
@@ -2,18 +2,26 @@ const { ethers } = require('ethers');
 const fs = require('fs');
 const proof = require('./proof.json');
 const publicSignals = require('./public.json');
+const snarkjs = require('snarkjs');
+const vkey = require('./verification_key.json');
 
 require('dotenv').config();
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const contractAddress = '';
+const contractAddress = '0x47DE457fc5bB3D07263dFD67774E66cfCd0C758f';
 const contractJson = JSON.parse(fs.readFileSync('Groth16Verifier.json', 'utf8'));
 const abi = contractJson.abi;
 
 const contract = new ethers.Contract(contractAddress, abi, wallet);
 
 async function verifyProof() {
+
+    const proofObject = proof;
+    const publicSignalsArray = publicSignals;
+
+    const res = await snarkjs.groth16.verify(vkey, publicSignalsArray, proofObject);
+    console.log('Proof verification result:', res);
     try {
         const pi_a = [proof.pi_a[0], proof.pi_a[1]];
         const pi_b = [
