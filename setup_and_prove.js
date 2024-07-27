946d83638c87ff5c4a2cb127ff92aebb7a586c67
lancenonce <lancendavis47@gmail.com>
2024-07-27 08:29:14 +0000

    cleanup

--- setup_and_prove.js
+++ setup_and_prove.js
@@ -4,7 +4,6 @@ const snarkjsPath = path.resolve('node_modules/.bin/snarkjs');
 const fs = require('fs');
 
 async function setupAndProve() {
-    // Trusted setup
     execSync(`${snarkjsPath} powersoftau new bn128 12 powersOfTau28_hez_final_10.ptau -v`, { stdio: 'inherit' });
     execSync(`${snarkjsPath} powersoftau contribute powersOfTau28_hez_final_10.ptau powersOfTau28_hez_final_11.ptau --name="First contribution" -v`, { stdio: 'inherit' });
     execSync(`${snarkjsPath} powersoftau prepare phase2 powersOfTau28_hez_final_11.ptau powersOfTau28_hez_final.ptau -v`, { stdio: 'inherit' });
@@ -12,12 +11,10 @@ async function setupAndProve() {
     execSync(`${snarkjsPath} zkey contribute polynomial_0000.zkey polynomial_final.zkey --name="First contribution" -v`, { stdio: 'inherit' });
     execSync(`${snarkjsPath} zkey export verificationkey polynomial_final.zkey verification_key.json`, { stdio: 'inherit' });
 
-    // Generate witness
-    const input = { x: 1 };  // Define the input value for x here
+    const input = { x: 1 }; 
     fs.writeFileSync('input.json', JSON.stringify(input));
     execSync(`node polynomial_js/generate_witness.js polynomial_js/polynomial.wasm input.json witness.wtns`, { stdio: 'inherit' });
 
-    // Generate proof
     execSync(`${snarkjsPath} groth16 prove polynomial_final.zkey witness.wtns proof.json public.json`, { stdio: 'inherit' });
 }
 
--- verify_proof.js
+++ verify_proof.js
@@ -3,12 +3,11 @@ const fs = require('fs');
 const proof = require('./proof.json');
 const publicSignals = require('./public.json');
 
-// Load environment variables from .env file
 require('dotenv').config();
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const contractAddress = '0x8Cd11739744f62C502166623A813Cc73A038b328';
+const contractAddress = '';
 const contractJson = JSON.parse(fs.readFileSync('Groth16Verifier.json', 'utf8'));
 const abi = contractJson.abi;
 
@@ -16,7 +15,6 @@ const contract = new ethers.Contract(contractAddress, abi, wallet);
 
 async function verifyProof() {
     try {
-        // Convert proof values to string
         const pi_a = [proof.pi_a[0], proof.pi_a[1]];
         const pi_b = [
             [proof.pi_b[0][0], proof.pi_b[0][1]],
@@ -25,7 +23,6 @@ async function verifyProof() {
         const pi_c = [proof.pi_c[0], proof.pi_c[1]];
         const pubSignals = publicSignals.map(String);
 
-        // Call the verifyProof function
         const result = await contract.verifyProof(pi_a, pi_b, pi_c, pubSignals);
         console.log('Proof verification result:', result);
     } catch (error) {
